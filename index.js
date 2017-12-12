6fbe791ff291dae224bf13057f270eca00a112c5
GhulamRasool <iamgrasool@gmail.com>
2017-12-12 20:11:16 +0500

    updates

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const config = require('./config/database');
-var port = 8088;
+const path = require('path');
+
+var port = 8090;
 // mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
 mongoose.connect(config.uri,{ useMongoClient: true }, (err)=>{
 	if(err){
@@ -18,10 +20,18 @@ mongoose.connect(config.uri,{ useMongoClient: true }, (err)=>{
 
 // use ng build to work with angular
 // in angular
+// and then add routes for angular collabirating with node 
+
+app.use(express.static(__dirname+'/client/dist/'));
 
 app.get('/', (req, res)=> {
-res.send('<h3>Hello Worlds: PHP to node</h3>');
-});
+	res.sendFile(path.join(__dirname+'/client/dist/index.html'));
+	});
+
+
+// app.get('/', (req, res)=> {
+// res.send('<h3>Hello Worlds: PHP to node</h3>');
+// });
 
 app.listen(port, ()=>{
 	console.log('listening on port '+port);
